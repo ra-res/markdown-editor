b897636e7a601aa882c68a7d7575de570a79377c
nifty <purtan.rares@yahoo.com>
2021-10-24 14:39:09 +0100

    Implement core functionality

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,10 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState, ChangeEventHandler } from 'react'
 
+import { sampleText } from '../sampleText'
 
 const Editor: FC = () => {
+    const [text, setText] = useState<string>(sampleText)
+
+    const handleChange: ChangeEventHandler<HTMLTextAreaElement> = e => {
+        const _text = e.target.value
+        setText(_text)
+    }
+
+    useEffect(() => {
+        if (text) {
+            localStorage.setItem('text', text)
+        }
+    }, [text])
+
     return (
         <div className="editor">
-            <textarea rows={35} />
+            <textarea onChange={handleChange} value={text} rows={35} />
         </div>
     )
 }
--- src/Components/Preview.tsx
+++ src/Components/Preview.tsx
@@ -2,7 +2,19 @@ import React, { FC } from 'react'
 import marked from 'marked'
 
 const Preview: FC = () => {
-    return <div className="preview"></div>
+    const text = localStorage.getItem('text')
+    const render = (text: string | null) => {
+        if (text) {
+            const __html = marked(text, { sanitize: true })
+            return { __html }
+        }
+    }
+
+    return (
+        <div className="preview">
+            <div dangerouslySetInnerHTML={render(text)} />
+        </div>
+    )
 }
 
 export default Preview
